ed23387e88dae5204c80d9870adfa3ba69f4baee
SungYeolha <tjddufgk@gmail.com>
2021-02-17 05:10:03 +0900

    BrowserRouter -> HashRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Redirect,
   Switch,
-} from "react-router-dom";
-import Home from "Routes/Home";
-import TV from "Routes/TV";
-import Search from "Routes/Search";
-import Detail from "Routes/Detail";
-import Header from "./Header";
+} from 'react-router-dom';
+import Home from 'Routes/Home';
+import TV from 'Routes/TV';
+import Search from 'Routes/Search';
+import Detail from 'Routes/Detail';
+import Header from './Header';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
